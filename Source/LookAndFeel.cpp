d1091a575911196950e115fb259dc25546786760
Sean Enderby <sean.enderby@gmail.com>
2016-12-13 08:19:12 +0000

    some comments and tweaks

--- Source/LookAndFeel.cpp
+++ Source/LookAndFeel.cpp
@@ -1,7 +1,8 @@
 #include "LookAndFeel.h"
 
 ChristmasLookAndFeel::ChristmasLookAndFeel (const ImageData& imageData)
-    : rotarySliderWidth (imageData.rotarySliderImageWidth), 
+    // We initialise all the image data in the initialisation list.
+    : rotarySliderWidth (imageData.rotarySliderImageWidth),  
       rotarySliderHeight (imageData.rotarySliderImageHeight),
       rotarySliderImage (Drawable::createFromImageData (imageData.rotarySliderImageData,
                                                         imageData.rotarySliderImageSize))
@@ -19,15 +20,20 @@ void ChristmasLookAndFeel::drawRotarySlider (Graphics &g,
                                              float rotaryEndAngle,
                                              Slider &slider)
 {
+    // Work out the angle of rotation to apply to the slider image.
     float angleRange = rotaryEndAngle - rotaryStartAngle;
     float angle = sliderPosProportional * angleRange + rotaryStartAngle;
     
+    // Find the center of the region we are drawing into.
     float centreX = width / 2.0f;
     float centreY = height / 2.0f;
     
+    // Define a transform which will scale our slider image to
+    // the size of the region and rotate it to the correct angle.
     AffineTransform transform = AffineTransform::scale (width / rotarySliderWidth, 
                                                         height / rotarySliderHeight);
     transform = transform.rotated (angle, centreX, centreY);
         
+    // Draw the slider image with the transform applied.
     rotarySliderImage->draw (g, 1.0f, transform);
 }
--- Source/LookAndFeel.h
+++ Source/LookAndFeel.h
@@ -3,19 +3,26 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+// A class which describes how the JUCE GUI Components are drawn.
 class ChristmasLookAndFeel : public LookAndFeel_V2
 {
 public:
+    // A little structure which will contain pointers to 
+    // the image data for our sliders.
     struct ImageData
     {
-        const void *rotarySliderImageData;
-        int rotarySliderImageSize;
-        float rotarySliderImageWidth, rotarySliderImageHeight;
+        const void *rotarySliderImageData; // The image data.
+        int rotarySliderImageSize; // The number of bytes in the image data.
+
+        // The dimensions of the actual image.
+        float rotarySliderImageWidth, rotarySliderImageHeight; 
     };
     
+    // The constructor takes a reference to the image data structure.
     ChristmasLookAndFeel (const ImageData& imageData);
     ~ChristmasLookAndFeel();
     
+    // Our sliders will call this function to draw themselves.
     void drawRotarySlider (Graphics &g,
                            int x, int y, int width, int height,
                            float sliderPosProportional,
@@ -24,6 +31,7 @@ public:
                            Slider &slider) override;
                            
 private:
+    // Variables in which to store the image data.
     float rotarySliderWidth, rotarySliderHeight;
     ScopedPointer <Drawable> rotarySliderImage;
     
